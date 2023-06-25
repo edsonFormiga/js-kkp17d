484e31e373deaf46be0eca50114b8cc7c6d30529
edsonFormiga <137646154+edsonFormiga@users.noreply.github.com>
2023-06-25 16:36:50 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let senhaDoWiFi = 159000;
+
+  let senhaDigitada1 = 169000;
+  let senhaDigitada2 = 159000;
+  let senhaDigitada3 = '159000';
+  let senhaDigitada4 = 159000;
+  let verificacao1 = senhaDigitada1 === senhaDoWiFi;
+
+  console.log(verificacao1); // false
